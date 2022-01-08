dc983c86e8238950e4a876a84e18e25965f022d1
Dazzle2009 <82277230+Dazzle2009@users.noreply.github.com>
2022-01-08 11:22:03 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,28 @@
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0um0-wChQ8L2RlRFxtq6trmsK_-dogVM",
+    authDomain: "kwitter-website-3f25b.firebaseapp.com",
+    databaseURL: "https://kwitter-website-3f25b-default-rtdb.firebaseio.com",
+    projectId: "kwitter-website-3f25b",
+    storageBucket: "kwitter-website-3f25b.appspot.com",
+    messagingSenderId: "44807958226",
+    appId: "1:44807958226:web:5bea1f7599f485babc470b",
+    measurementId: "G-5EDHW43XTE"
+  };
+  // Initialize Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0um0-wChQ8L2RlRFxtq6trmsK_-dogVM",
+    authDomain: "kwitter-website-3f25b.firebaseapp.com",
+    databaseURL: "https://kwitter-website-3f25b-default-rtdb.firebaseio.com",
+    projectId: "kwitter-website-3f25b",
+    storageBucket: "kwitter-website-3f25b.appspot.com",
+    messagingSenderId: "44807958226",
+    appId: "1:44807958226:web:5bea1f7599f485babc470b",
+    measurementId: "G-5EDHW43XTE"
+  };
+
+    //Start code
+
+    //End code
+  
